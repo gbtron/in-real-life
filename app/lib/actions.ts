738c297c93c6594ffcd2d275180c2aa10d077034
Joseph <josephrfuentes@gmail.com>
2024-08-05 15:04:04 -0400

    send form to db with unique phone number req. provide error messages for each field.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,17 +19,17 @@ const FormSchema = z.object({
     id: z.string(),
     name: z.string({
         invalid_type_error: 'Please enter your full name.'
-    }),
+    }).min(5).regex(/^[\p{L}\s'-]*$/u).max(30),
     phone: z.string({
-        invalid_type_error: 'Please enter your phone number.'
-    }),
+        invalid_type_error: 'Please enter a valid phone number.'
+    }).min(10).max(29).regex(/^[\(\+]?\d{0,14}\)?[\s\d-]{9,28}$/),
     message: z.string({
         invalid_type_error: 'Please enter the message you would like to send us.'
-    }),
-    date: z.string(),
+    }).max(200),
+    date: z.string().date(),
     email: z.string({
-        invalid_type_error: "Please enter your email address."
-    }),
+        invalid_type_error: "Please enter a valid email address."
+    }).email().max(50),
 })
 
 const CreateLead = FormSchema.omit({id: true, date: true}); 
@@ -41,26 +41,31 @@ export async function sendMessage(prevState: State, formData: FormData) {
         phone: formData.get('phone'),
         message: formData.get('message')
     })
-
     if (!validatedFields.success) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Missing Fields. Failed to Create Lead Message'
+            message: 'Some of the fields are invalid. Please correct them and try again.', 
         }
     }
-    console.log(validatedFields)
-    const date = new Date().toISOString().split('T')[0];
-    // try {
-    //     await sql`
-    //     INSERT INTO leads(name, email, phone, message, date)
-    //     VALUES (${name}, ${email}, ${phone}, ${message}, ${date})
-    //     `;  
-    // } catch (error){
-    //     return {
-    //         message: "Database Error: Failed to create lead message"
-    //     }
-    // }
+
+    const {name, email, phone, message} = validatedFields.data
     
-    revalidatePath('/contact');
-    redirect('/contact');
+    const date = new Date().toISOString().split('T')[0];
+    try {
+        await sql`
+        INSERT INTO leads(name, email, phone, message, date)
+        VALUES (${name}, ${email}, ${phone}, ${message}, ${date})
+        `;  
+    } catch (error){
+        console.error("Database error", error);
+        return {
+            errors: {},
+            message: "The message could not be sent."
+        }
+    }
+    return {
+        errors:{},
+        message: "The message was sent successfully."
+    }
+    
 }
--- app/ui/submit-form.tsx
+++ app/ui/submit-form.tsx
@@ -1,49 +1,95 @@
 "use client"
 
 import { sendMessage, State } from '@/app/lib/actions'; 
-import { useActionState } from 'react';
-
-const initialState= {
-    firstName: '',
-    lastName: '',
-    email: '',
-    message: ''
-}
+import { useActionState, useState } from 'react';
 
 export default function Form() {
     const initialState: State = { message: null, errors: {} };
     const [state, formAction] = useActionState(sendMessage, initialState);
+    const [phone, setPhone] = useState('');
+
+    const onPhoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        if (value.length === 4 && !value.includes('(') && !value.includes('+')) {
+            setPhone(`${value.slice(0,3)}-${value.slice(-1)}`);
+        } else if (value.length === 10 && !value.includes('(') && !value.includes('+')) {
+            setPhone(`(${value.slice(0,3)}) ${value.slice(4,7)}-${value.slice(7)}`);
+        }
+        else {
+            setPhone(value);
+        }
+    }
 
     return (
-        <form className="" action={formAction}>
+        <form action={formAction}>
             <div className="bg-white p-8 flex flex-col rounded-md border-black">
                 <h1 className="text-2xl text-slate-900 font-semibold">How can we reach you?</h1>
-                <div className="mb-8 text-slate-500"> Let us know your preferred method of communication </div>
-                <div className="flex flex-col md:flex-row">
+                <div className="text-slate-500"> Let us know your preferred method of communication </div>
+                <div className="mt-8 flex flex-col md:flex-row">
                     <label htmlFor="name" className="font-semibold text-sm inline-block w-16">Name</label>
-                    <input className="md:ml-16 mb-4 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" placeholder="Jane Diaz" id="name" name="name" />
+                    <input aria-describedby="name-error" className="md:ml-16 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" placeholder="Jane Diaz" id="name" name="name" />
                 </div>
-                <div className="md:flex">
+                <div id="name-error" className="text-red-600" aria-live="polite" aria-atomic="true">
+                    {state?.errors?.name?.[0] &&
+                      state.errors.name.map((error:string) => (
+                        <p className="text-red-600 my-2 text-sm" key={error}>
+                            {error.replace('String', 'Your name').replace('must', 'should')}
+                        </p>
+                    ))}
+                </div>
+                <div className="md:flex mt-4">
                     <label className="font-semibold text-sm inline-block w-16" htmlFor="email">Email</label>
-                    <input className="md:ml-16 mb-4 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" type="email" placeholder="jane@email.com" id="email" name="email"  />
+                    <input aria-describedby="email-error" className="md:ml-16 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" placeholder="jane@email.com" id="email" name="email"  />
                 </div>
-                <div className="flex flex-col md:flex-row">
+                <div id="email-error" className="text-red-600" aria-live="polite" aria-atomic="true">
+                    {state?.errors?.email?.[0] &&
+                      state.errors.email.map((error:string) => (
+                        <p className="text-red-600 my-2 text-sm" key={error}>
+                            {error.replace('String', 'Your email').replace('must', 'should')}
+                        </p>
+                    ))}
+                </div>
+                <div className="flex flex-col md:flex-row mt-4">
                     <label className="font-semibold text-sm inline-block w-16" htmlFor="email">Phone</label>
-                    <input className="md:ml-16 mb-4 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" type="tel" placeholder="+3(333)333-3333" id="phone" name="phone"  />
+                    <input className="md:ml-16 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" type="tel" placeholder="(333) 333-3333" value={phone} onChange={onPhoneChange} id="phone" name="phone"  />
                 </div>
-                <div className="flex flex-col md:flex-row mb-8">
+                <div id="phone-error" className="text-red-600" aria-live="polite" aria-atomic="true">
+                    {state?.errors?.phone?.[0] &&
+                      state.errors.phone.map((error:string) => (
+                        <p className="text-red-600 my-2 text-sm" key={error}>
+                            {error.replace('String', 'Your phone number').replace('must', 'should')}
+                        </p>
+                    ))}
+                </div>
+                <div className="flex flex-col md:flex-row mt-4">
                     <label className="font-semibold text-sm inline-block w-16" htmlFor="message">Message</label>
                     <textarea className="md:ml-16 w-full md:w-80 bg-slate-100 rounded-sm px-2 py-1" placeholder="Tell us if you are a parent, young adult, or counselor " id="message" name="message"  />
                 </div>
-
-                <div className="justify-end flex md:justify-start">
+                <div id="message-error" className="text-red-600" aria-live="polite" aria-atomic="true">
+                    {state?.errors?.message?.[0] &&
+                      state.errors.message.map((error:string) => (
+                        <p className="text-red-600 my-2 text-sm" key={error}>
+                            {error.replace('String', 'Your message').replace('must', 'should')}
+                        </p>
+                    ))}
+                </div>
+                <div className="mt-4">
+                    {state.message!.includes("could not be sent") && 
+                        <p aria-live="polite" role="status" className="text-red-600">
+                            {state.message}
+                        </p>
+                    }
+                    {state.message!.includes("success") && 
+                        <p aria-live="polite" role="status" className="text-green-600">
+                            {state?.message}
+                        </p>
+                    }
+                </div>
+                <div className="justify-end flex md:justify-start mt-8">
                     <button type="submit" className="bg-indigo-600 text-white px-4 rounded-xl w-20 ml-[23rem] hover:bg-black hover:text-slate-100">
                         Send
                     </button>
                 </div>
-                <p aria-live="polite" role="status" className="text-red-600">
-                  
-                </p>
             </div>
         </form>
     )
