18e2c5c921500a3e753f1d36ea69bf9700011047
Geeb <josephrfuentes@gmail.com>
2024-09-07 16:00:08 -0400

    log in/out button

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,11 @@ import { Footer } from "../app/ui/Footer";
 import { Banner } from "./ui/Banner";
 import {ScrollableCards} from '@/app/ui/Cards'
 import { bookmania } from '@/app/ui/fonts'
-import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser()
-  if (isLoading) return <>Loading</>
-  if (error) return <>{error.message}</>
   return (
       <div className='dark:bg-gray-800 text-center'>
-        <Banner user={user}/>
+        <Banner/>
         <div className={`${bookmania.className} font-bold italic text-2xl`}>
           Welcome,
         </div>
--- app/ui/Banner.tsx
+++ app/ui/Banner.tsx
@@ -1,24 +1,28 @@
 'use client'
 import { FaSun, FaMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
-import { UserProfile } from "@auth0/nextjs-auth0/client";
-import { bookmania, acumen  } from "@/app/ui/fonts";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { acumen } from "@/app/ui/fonts";
 
-export function Banner({user}:UserProfile) {
+export function Banner() {
+    const { user, error, isLoading } = useUser()
+    
     const {theme, setTheme} = useTheme()
     let themeNot = 'dark'
     let icon = <FaMoon/>
-    console.log('user-', user)
 
     if (theme == 'dark') {
         themeNot = 'light'
         icon = <FaSun/>
     }
-    
+
     return (
         <div className={`flex justify-between px-12 pt-12 pb-12 ${acumen.className} font-bold`}>
             <button onClick={()=>setTheme(themeNot)}>{icon}</button>
-            {user !== undefined ? <a href='/api/auth/logout' >Log Out</a> : <a href='/api/auth/login'>Log In</a>}
+            {user !== undefined 
+                ? (!isLoading && !error) && <a href='/api/auth/logout' >Log Out</a> 
+                : <a href='/api/auth/login'>Log In</a>
+            }
             
         </div>
     )
