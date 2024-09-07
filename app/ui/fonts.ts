887cd213192c2e8cd461b12379df25951fc243c2
Geeb <josephrfuentes@gmail.com>
2024-09-06 22:40:27 -0400

    add bookmania fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter, Cardo, Hanken_Grotesk, Handlee } from "next/font/google";
+import localFont from 'next/font/local'
 
 export const inter = Inter({ subsets: ["latin"] });
 
@@ -16,3 +17,58 @@ export const handlee = Handlee({
     weight: ['400'],
     subsets: ["latin"]
 });
+
+export const bookmania = localFont({
+    src: [
+        {
+            path: '../../public/fonts/Bookmania-Black.woff2', 
+            weight: '900', 
+            style: 'normal'
+        },
+        {
+            path: '../../public/fonts/Bookmania-BlackItalic.woff2', 
+            weight: '900', 
+            style: 'italic'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-Bold.woff2', 
+            weight: '700', 
+            style: 'normal'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-BoldItalic.woff2', 
+            weight: '700', 
+            style: 'italic'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-Light.woff2', 
+            weight: '100', 
+            style: 'normal'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-LightItalic.woff2', 
+            weight: '100', 
+            style: 'italic'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-Regular.woff2', 
+            weight: '400', 
+            style: 'normal'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-RegularItalic.woff2', 
+            weight: '400', 
+            style: 'italic'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-Semibold.woff2', 
+            weight: '500', 
+            style: 'normal'
+        }, 
+        {
+            path: '../../public/fonts/Bookmania-SemiboldItalic.woff2', 
+            weight: '500', 
+            style: 'italic'
+        }
+    ]
+})
