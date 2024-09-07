4753054015fbf1e583ac149d03a03d86a080d7bb
Geeb <josephrfuentes@gmail.com>
2024-09-07 16:53:16 -0400

    conditionally render text for logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
 'use client'
 import { Footer } from "../app/ui/Footer";
 import { Banner } from "./ui/Banner";
-import {ScrollableCards} from '@/app/ui/Cards'
-import { bookmania } from '@/app/ui/fonts'
+import { GuestGreeting } from "@/app/ui/GuestGreeting"
+import { ScrollableCards } from '@/app/ui/Cards'
+import { acumen } from '@/app/ui/fonts'
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { Spinner } from "@/app/ui/Spinner"
 
 export default function Home() {
+  const { user, error, isLoading } = useUser()
+  if (!isLoading && !error ) {
+    if (user !== undefined) {
+
+    }
+  } 
   return (
-      <div className='dark:bg-gray-800 text-center'>
+      <div className={`text-center ${acumen.className}`}>
         <Banner/>
-        <div className={`${bookmania.className} font-bold italic text-2xl`}>
-          Welcome,
-        </div>
-        <div className='pb-2'>
-          we look forward to serving you well 
-        </div>
-        <a href='/api/auth/login' className='py-1 px-2 bg-slate-200 dark:bg-slate-700 rounded-md hover:bg-blue-100'>
-          Begin
-        </a>
+        {isLoading && <Spinner/>}
+        {error && <div className="text-red-700">{error.message} </div>}
+        {user === undefined 
+          ? <GuestGreeting/> 
+          : <div className='text-2xl'> Hello, {user.name}</div>
+        }
         <ScrollableCards/>
         <Footer/>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,9 +14,39 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors : {
-        'camo': '#efeedb', 
-        'lightTan': '#EAE2D4',
-        'burntOrange': '#DD5C3D',
+        'camo': {
+          100: '#efeedb',
+          200: '#e7e7ce',
+          300: '#dedfc2',
+          400: '#d5d7b5',
+          500: '#cdcfaa',
+          600: '#c4c79e',
+          700: '#bcbf93',
+          800: '#b3b788',
+          900: '#a9ae7e'
+        }, 
+        'burntOrange': {
+          100: '#DD5C3D',
+          200: '#D4563A',
+          300: '#CC5138',
+          400: '#C44C35',
+          500: '#BC4732',
+          600: '#B4422F',
+          700: '#AC3D2C',
+          800: '#A4382A',
+          900: '#9C3327'
+        },
+        'brown': {
+          100: '#534742',
+          200: '#5E4D48',
+          300: '#6A5950',
+          400: '#776459',
+          500: '#847162',
+          600: '#927D6B',
+          700: '#A08A74',
+          800: '#AE977E',
+          900: '#BCA588'
+        }
       }
     },
   },
