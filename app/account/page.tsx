5245098c44b8e9304a372444dcc589fcd8fdcfb4
The Real Mi <miaolmedillo@gmail.com>
2024-09-08 18:09:12 -0400

    bringing in auth0 for account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,29 @@
-"use client";
+'use client'
 import { Banner } from "../ui/Banner";
 import { Footer } from "../ui/Footer";
 import { mockUser } from "../lib/data";
 import { useState } from "react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Account() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-
+  const { user, error, isLoading } = useUser();
+  if (!user) {
+    return (<>
+      <Banner/>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold mb-4">You must log in to see this page</h1>
+          <a href="/api/auth/login" className="text-burntOrange-700 underline">
+            Log in
+          </a>
+        </div>
+      </div>
+      <Footer />
+      </>
+    );
+  }
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -27,16 +43,18 @@ export default function Account() {
     "305-305-3055",
     "ottoPilot",
   ];
+  
   return (
     <>
       {/* Make banner conditionally display log out button if a user is logged in */}
       <Banner />
+      { user &&
       <div className="px-4 md:flex">
         <div className="my-3 p-5 bg-gray-100 rounded-md md:flex-grow mx-3">
           {/* Add custom font */}
           <h1 className={`mb-4 text-xl md:text-2x1`}>Account Info</h1>
           <div className="divide-y divide-gray-300">
-            {acctInfo.map((user, i) => (
+            {acctInfo.map((map, i) => (
               <div className="py-2 grid grid-cols-2" key={i}>
                 <div>{acctInfo[i]}</div>
                 <div className="text-right">{currentUser[i]}</div>
@@ -44,8 +62,7 @@ export default function Account() {
             ))}
             <div className="py-2 grid grid-cols-2">
               <div>Password</div>
-              {/* display hidden password Use eye icon button to show password */}
-              <div className="text-right">
+                <div className="text-right">
                 {passwordVisible ? mockUser[0].password : "**********"}
 
                 <button
@@ -64,7 +81,7 @@ export default function Account() {
           </div>
           {/* Link to form to change account info */}
           <div className="text-right">
-            <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
+            <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded">
               Edit account info
             </button>
           </div>
@@ -77,7 +94,7 @@ export default function Account() {
             <div className="py-2 grid grid-cols-2">
               <div>Member Since</div>
               <div className="text-right">
-                9/2/2024<></>
+                9/2/2024
               </div>
             </div>
             <div className="py-2 grid grid-cols-2">
@@ -91,12 +108,13 @@ export default function Account() {
           </div>
 
           <div className="text-right md:absolute bottom-4 right-4">
-            <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
+            <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded">
               Manage Membership
             </button>
           </div>
         </div>
-      </div>
+      </div>}
+      
       <Footer />
     </>
   );
