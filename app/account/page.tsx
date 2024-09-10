9c12ad0adef29273d55d8ac1acde24bf65236491
The Real Mi <miaolmedillo@gmail.com>
2024-09-09 21:11:29 -0400

    adding metadata and loading screen

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,11 +3,12 @@ import { Banner } from "../ui/Banner";
 import { Footer } from "../ui/Footer";
 import { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { Spinner } from "@/app/ui/Spinner";
 
 const UserInfoRow = ({ label, value }) => (
   <div className="py-2 grid grid-cols-2">
     <div>{label}</div>
-    <div className="text-right">{value}</div>
+    <div className="text-right overflow-x-auto">{value}</div>
   </div>
 );
 
@@ -17,6 +18,7 @@ export default function Account() {
     return (
       <>
         <Banner />
+        {isLoading && <Spinner />}
         <div className="flex items-center justify-center min-h-screen">
           <div className="text-center">
             <h1 className="text-2xl font-semibold mb-4">
@@ -34,32 +36,41 @@ export default function Account() {
       </>
     );
   }
- 
+
+  const app_metadata = user?.app_metadata || {};
+  const user_metadata = user?.user_metadata || {};
+
   return (
     <>
       <Banner />
       {user && (
         <div className="px-4 md:flex">
-          <div className="my-3 p-5 bg-gray-100 rounded-md md:flex-grow mx-3">
+          <div className="my-3 p-5 bg-gray-100 rounded-md dark:bg-gray-500 md:flex-grow mx-3">
             {/* Add custom font */}
             <h1 className={`mb-4 text-xl md:text-2x1`}>Account Info</h1>
             <div className="divide-y divide-gray-300">
-              <UserInfoRow label="Account Type" value="Use app_metadata here" />
+              <UserInfoRow
+                label="Account Type"
+                value={app_metadata.account_type || "N/A"}
+              />
               <UserInfoRow label="Name" value={user.name} />
               <UserInfoRow label="Email" value={user.email} />
-              <UserInfoRow label="Phone Number" value={user.phone || "N/A"} />
+              <UserInfoRow
+                label="Phone Number"
+                value={user_metadata.phone || "N/A"}
+              />
               <UserInfoRow label="Username" value={user.nickname} />
               <UserInfoRow label="Password" value="*********" />
             </div>
             {/* Link to form to change account info */}
             <div className="text-right">
-              <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded">
+              <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded dark:text-white dark:border-white">
                 Edit account info
               </button>
             </div>
           </div>
 
-          <div className="my-3 p-5 bg-gray-100 rounded-md md:flex-grow mx-3 md:relative">
+          <div className="my-3 p-5 bg-gray-100 rounded-md dark:bg-gray-500 md:flex-grow mx-3 md:relative">
             {/* Add custom font */}
             <h1 className={`mb-4 text-xl md:text-2x1`}>Membership</h1>
             <div className="divide-y divide-gray-300">
@@ -72,7 +83,7 @@ export default function Account() {
             </div>
 
             <div className="text-right md:absolute bottom-4 right-4">
-              <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded">
+              <button className="bg-transparent hover:bg-burntOrange-700 text-burntOrange-700 font-semibold hover:text-white py-2 px-4 border border-burntOrange-700 hover:border-transparent rounded dark:text-white dark:border-white">
                 Manage Membership
               </button>
             </div>
