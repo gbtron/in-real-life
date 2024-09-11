a861d6eb3303bba8890943c4bd98adef910645ea
Joseph <josephrfuentes@gmail.com>
2024-09-10 23:46:46 -0400

    attempt to trigger button press

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,19 +1,40 @@
-import { expect, test, vi } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { expect, test, vi, describe, beforeAll } from 'vitest'
+import { screen, waitFor } from '@testing-library/react'
 import Landing from '@/app/page'
+import { setup } from './setup'
 
-test('Landing Page', ()=> {
-    vi.mock('@auth0/nextjs-auth0/client', ()=> ({
-        useUser: vi.fn(() => ({
-            user:undefined, 
-            error:undefined, 
-            isLoading:undefined
+describe('The landing page', () => {
+    beforeAll(() => {
+        vi.mock('@auth0/nextjs-auth0/client', ()=> ({
+            useUser: vi.fn(() => ({
+                user:undefined, 
+                error:undefined, 
+                isLoading:undefined
+            }))
         }))
-    }))
+    
+    })
 
-    render(
-        <Landing/>
-    )
-    const mainElement = screen.getByRole('main')
-    screen.debug()
+    test('dark mode toggle', async ()=> {
+        const { user } = setup(<Landing/>)
+
+        const header = screen.getByRole('banner')
+        expect(header.getAttribute('aria-label')).toBe('Moon icon and log in buttons.')
+        
+        const darkModeButton = screen.getByRole('button')
+        await user.pointer([
+            {keys:'[TouchA>]', target:darkModeButton}, 
+            {keys:'[/TouchA]'}
+        ])
+        
+        await waitFor(()=> {
+            expect(header.getAttribute('aria-label')).toBe('Sun icon and log in buttons.')
+        })
+
+        //expect(screen.getByRole('link'))
+        //expect(await screen.findByText('Hello, ') ).toBeInTheDocument()
+        expect(screen.getByRole('main'))
+        expect(screen.getByRole('contentinfo'))
+        screen.debug()
+    })
 })
