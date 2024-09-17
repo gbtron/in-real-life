3675769888968d22a5359de864eaebe2bb07b1c5
Geeb <josephrfuentes@gmail.com>
2024-09-17 18:03:48 -0400

    import carousel react plugin

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,8 @@ import { Spinner } from "@/app/ui/Spinner"
 
 export default function Landing() {
   const { user, error, isLoading } = useUser()
-  if (!isLoading && !error ) {
-    if (user !== undefined) {
-
-    }
-  } 
   return (
-      <div className='text-center'>
+      <div className='text-center pt-20'>
         <main>
           {isLoading && <Spinner/>}
           {error && <div className="text-red-700">{error.message} </div>}
--- app/ui/Cards.tsx
+++ app/ui/Cards.tsx
@@ -1,25 +1,59 @@
 import { mockEvents } from "@/app/lib/data"
+import Carousel from "react-multi-carousel"
+import 'react-multi-carousel/lib/styles.css';
 
 export function ScrollableCards() {
     const getParticipantCount = () => (5)
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     return (
-        <div className='width-full overflow-x-auto px-24 py-2 mt-4'>
-            <div className='flex width-full'>
-                {mockEvents.map((event, i)=> (
-                    <div className='w-full h-4/6 py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
-                        <div className='flex flex-row justify-between'>
-                            <div>{event.membersEvent && 'Members Only'}</div>
-                            <div>{event.sign}</div>
-                        </div>
-                        <div className='flex flex-col pt-4 text-center'>
-                            <div>{event.title}</div>
-                            <div>on {event.date.toDateString()}</div>
-                        </div>
-                        <div className='pt-8'>{getParticipantCount()}</div>
-                    </div>
-                ))}
+       <Carousel 
+            responsive={responsive} 
+            swipeable={true}
+            draggable={true}
+            showDots={true}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            deviceType="mobile"
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+        >
+        {mockEvents.map((event, i)=> (
+            <div className='w-1/2 h-4/6 py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
+                <div className='flex flex-row justify-between'>
+                    <div>{event.membersEvent && 'Members Only'}</div>
+                    <div>{event.sign}</div>
+                </div>
+                <div className='flex flex-col pt-4 text-center'>
+                    <div>{event.title}</div>
+                    <div>on {event.date.toDateString()}</div>
+                </div>
+                <div className='pt-8'>{getParticipantCount()}</div>
             </div>
-
-        </div>
+        ))}
+        </Carousel>
     )
 }
