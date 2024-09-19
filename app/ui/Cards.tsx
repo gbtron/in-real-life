84cc88cb2156156663f53d5d70dee6f890add8b2
Geeb <josephrfuentes@gmail.com>
2024-09-19 16:00:44 -0400

    adjust props carousel

--- app/ui/Cards.tsx
+++ app/ui/Cards.tsx
@@ -8,43 +8,43 @@ export function ScrollableCards() {
         superLargeDesktop: {
           // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
-          items: 5
+          items: 3, 
+          partialVisibilityGutter: 40
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 3
+          items: 2, 
+          partialVisibilityGutter: 30
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
+          breakpoint: { max: 1024, min: 640 },
+          items: 1, 
+          partialVisibilityGutter: 30
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+          breakpoint: { max: 640, min: 0 },
+          items: 1, 
+          partialVisibilityGutter:30
         }
       };
     return (
        <Carousel 
-            arrows
             centerMode={true}
             responsive={responsive} 
             swipeable={true}
             draggable={false}
             showDots={true}
             ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            autoPlaySpeed={1000}
-            keyBoardControl={true}
-            customTransition="all .5"
-            transitionDuration={500}
             containerClass="carousel-container"
             removeArrowOnDeviceType={["tablet", "mobile"]}
             deviceType="mobile"
-            dotListClass="custom-dot-list-style"
+            dotListClass="custom-dot-list-style mt-20"
             itemClass="carousel-item-padding-40-px"
+            focusOnSelect={true}
+            rewindWithAnimation={true}
         >
         {mockEvents.map((event, i)=> (
-            <div className='w-full h-4/6 py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
+            <div className='h-full py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
                 <div className='flex flex-row justify-between'>
                     <div>{event.membersEvent && 'Members Only'}</div>
                     <div>{event.sign}</div>
@@ -53,7 +53,7 @@ export function ScrollableCards() {
                     <div>{event.title}</div>
                     <div>on {event.date.toDateString()}</div>
                 </div>
-                <div className='pt-8'>{getParticipantCount()}</div>
+                
             </div>
         ))}
         </Carousel>
