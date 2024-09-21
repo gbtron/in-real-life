a5ea3f46bf0d850f8589977110d8690f8d816af8
Joseph <josephrfuentes@gmail.com>
2024-09-21 17:00:40 -0400

    affects carousel styles

--- app/ui/Cards.tsx
+++ app/ui/Cards.tsx
@@ -1,9 +1,11 @@
 import { mockEvents } from "@/app/lib/data"
 import Carousel from "react-multi-carousel"
 import 'react-multi-carousel/lib/styles.css';
+import { sortEvents } from "@/app/lib/events";
+import { FaPerson } from 'react-icons/fa6'
+import { getParticipantCount } from "@/app/lib/events";
 
 export function ScrollableCards() {
-    const getParticipantCount = () => (5)
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
@@ -29,29 +31,35 @@ export function ScrollableCards() {
       };
     return (
        <Carousel 
-            infinite={true}
-            centerMode={true}
-            responsive={responsive} 
-            swipeable={true}
-            draggable={false}
-            ssr={true} // means to render carousel on server-side.
-            containerClass="carousel-container pt-4 sm:pt-12"
-            removeArrowOnDeviceType={["tablet", "mobile"]}
-            deviceType="mobile"
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-            focusOnSelect={true}
-            rewindWithAnimation={true}
+          arrows={false}
+          infinite={true}
+          centerMode={true}
+          responsive={responsive} 
+          swipeable={true}
+          draggable={false}
+          ssr={true} // means to render carousel on server-side.
+          containerClass="carousel-container h-80 py-12 sm:pt-12 mb-16"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style pb-4"
+          itemClass="carousel-item-padding-40-px"
+          focusOnSelect={true}
+          rewindWithAnimation={true}
+          sliderClass='h-full'
+          showDots={true}
         >
-        {mockEvents.map((event, i)=> (
-            <div className='h-full py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
-                <div className='flex flex-row justify-between'>
-                    <div>{event.membersEvent && 'Members Only'}</div>
+        {sortEvents(mockEvents).map((event, i)=> (
+            <div className='h-full mx-2 sm:mx-32 py-2 px-1 border-solid border-2 border-tangerine-400 dark:border-tangerine-100 rounded-md' key={i}>
+                <div className='flex text-left justify-between h-12'>
+                    <div>{event.membersOnly && 'Members Only'}</div>
                     <div>{event.sign}</div>
                 </div>
-                <div className='flex flex-col pt-4 text-center'>
+                <div className='flex flex-col pt-4 sm:pt-12 text-center h-1/2'>
                     <div>{event.title}</div>
-                    <div>on {event.date.toDateString()}</div>
+                    <div>on {event.date.toLocaleDateString(undefined, {month:'long', day:'numeric'})}</div>
+                </div>
+                <div className='pt-8 flex justify-center items-center'>
+                  {getParticipantCount()}
+                  <FaPerson/>
                 </div>
             </div>
         ))}
