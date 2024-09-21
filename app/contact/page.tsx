3de74c5d277b0c5f00abb57f41820b319c51c69a
Joseph <josephrfuentes@gmail.com>
2024-09-21 17:30:02 -0400

    adjusts contact form layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,11 +4,11 @@ import { Footer } from '../ui/Footer';
 
 export default async function Contact() {
     return (
-        <>
+        <div className='flex flex-col'>
             <section className="z-10 text-slate-600 dark:text-slate-200 text-center pt-10 pb-5 sm:pt-10 sm:pb-20 px-4 ">
                 <ContactForm/>
             </section>
             <Footer/>
-        </>
+        </div>
     )
 }
