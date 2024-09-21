09d45770940bbf316fc6b2f8428ec4a6d687b3da
Joseph <josephrfuentes@gmail.com>
2024-09-21 13:48:15 -0400

    bring up sematic html

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,17 @@ import { Spinner } from "@/app/ui/Spinner"
 export default function Landing() {
   const { user, error, isLoading } = useUser()
   return (
-      <div className='text-center pt-20'>
-        <main>
-          {isLoading && <Spinner/>}
-          {error && <div className="text-red-700" role="status">{error.message} </div>}
-          {user === undefined 
-            ? <GuestGreeting/> 
-            : <h1 className='text-2xl'> Hello, {user.name}</h1>
-          }
-          <ScrollableCards/>
-        </main>
-        <Footer/>
-      </div>
+    <>
+    <main className='text-center pt-20'>
+        {isLoading && <Spinner/>}
+        {error && <div className="text-red-700" role="status">{error.message} </div>}
+        {user === undefined 
+          ? <GuestGreeting/> 
+          : <h1 className='text-2xl'> Hello, {user.name}</h1>
+        }
+        <ScrollableCards/>
+      </main>
+      <Footer/>
+    </>
   );    
 }
