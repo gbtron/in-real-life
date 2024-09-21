ae5649be063a25588e6cc59042d161356760fc86
Joseph <josephrfuentes@gmail.com>
2024-09-21 16:23:30 -0400

    moves partcipant count getter to util file

--- app/lib/events.ts
+++ app/lib/events.ts
@@ -4,3 +4,5 @@ export const sortEvents = (mockEvents:Event[]) => {
     const parseDate = (date: Date) => ( Date.parse( date.toUTCString() ) )
     return mockEvents.toSorted( (a,b) => parseDate(a.date) - parseDate(b.date) )
 }
+
+export const getParticipantCount = () => (Math.round(Math.random()*20))
