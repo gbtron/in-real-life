096c1dc110b4b0d715cb5e8b516efb7c44d0b0f9
Joseph <josephrfuentes@gmail.com>
2024-09-27 13:53:38 -0400

    makes hover effects more subtle on nav bar

--- app/ui/Banner.tsx
+++ app/ui/Banner.tsx
@@ -20,7 +20,7 @@ export function Banner() {
     return (
         <header aria-label={`${themeLabel} and ${accountLabel} buttons.`} className='flex gap-4 sm:gap-12 items-center p-4 sm:p-12'>
             <button className="dark:hover:bg-brown-900 hover:bg-tangerine-200 rounded-xl p-1.5" onClick={toggleDarkMode}>{themeIcon}</button>
-            <div className="font-bold hover:bg-tangerine-200 rounded-md p-1">
+            <div className="hover:font-bold p-1">
                 {isLoggedIn
                     ? (!isLoading && !error) && <a href='/api/auth/logout'>Log Out</a> 
                     : <a href='/api/auth/login'>Log In</a>
