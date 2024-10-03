8ffcb8b25e0c0ecaee9207265c4002575df0bb3d
Joseph <josephrfuentes@gmail.com>
2024-10-02 23:11:38 -0400

    change image from background to next optimized image

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,11 +2,24 @@
 import ContactForm from '@/app/ui/contact-form';
 import { Footer } from '../ui/Footer';
 import React from 'react';
+import Image from 'next/image'
 
 export default async function Contact() {
     return (
         <>
-            <section className="z-10 text-slate-600 dark:text-slate-200 text-center pt-10 px-4 sm:pb-48">
+            <Image
+                fill={true}
+                style={{
+                    objectFit:'contain', 
+                    objectPosition:'right top'
+                }}
+                sizes="(max-width: 540px) 25vw, 70vw"
+                src='/assets/PalmLeaf.avif'
+                alt='palm tree'
+                priority={true}
+                className='z-0'
+            />
+            <section className="relative z-10 text-slate-600 dark:text-slate-200 text-center px-4 sm:pb-48">
                 <ContactForm/>
             </section>
             <Footer/>
