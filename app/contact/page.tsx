1822780804a502baa4bd6dcf3b7326016dea1a1e
Joseph <josephrfuentes@gmail.com>
2024-10-03 22:46:37 -0400

    restore image as background attribute

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,24 +2,11 @@
 import ContactForm from '@/app/ui/contact-form';
 import { Footer } from '../ui/Footer';
 import React from 'react';
-import Image from 'next/image'
 
 export default async function Contact() {
     return (
         <>
-            <Image
-                fill={true}
-                style={{
-                    objectFit:'contain', 
-                    objectPosition:'right top'
-                }}
-                sizes="100vw"
-                src='/assets/PalmLeaf.avif'
-                alt='palm tree'
-                priority={true}
-                className='z-0 w-1/2'
-            />
-            <section className="relative z-10 text-slate-600 dark:text-slate-200 text-center px-4">
+            <section className="relative z-10 text-slate-600 dark:text-slate-200 text-center px-4 pt-10">
                 <ContactForm/>
             </section>
             <Footer/>
