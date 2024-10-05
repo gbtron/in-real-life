207e4db2bf764204327bc20237239f72e0a6ec3a
Joseph <josephrfuentes@gmail.com>
2024-10-05 17:22:36 -0400

    removes unused fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,21 +1,9 @@
-import { Inter, Cardo, Hanken_Grotesk, Handlee } from "next/font/google";
+import { Cardo } from "next/font/google";
 import localFont from 'next/font/local'
-
-export const inter = Inter({ subsets: ["latin"] });
 
 export const cardo = Cardo({ 
     weight: ['400', '700'],
     subsets: ["greek"] 
-});
-
-export const hanken_grotesk = Hanken_Grotesk({
-    weight: ['500','900'],
-    subsets: ["latin"]
-});
-
-export const handlee = Handlee({
-    weight: ['400'],
-    subsets: ["latin"]
 });
 
 export const bookmania = localFont({
@@ -25,51 +13,51 @@ export const bookmania = localFont({
             weight: '900', 
             style: 'normal'
         },
-        {
-            path: '../../public/fonts/Bookmania-BlackItalic.woff2', 
-            weight: '900', 
-            style: 'italic'
-        }, 
-        {
-            path: '../../public/fonts/Bookmania-Bold.woff2', 
-            weight: '700', 
-            style: 'normal'
-        }, 
-        {
-            path: '../../public/fonts/Bookmania-BoldItalic.woff2', 
-            weight: '700', 
-            style: 'italic'
-        }, 
-        {
-            path: '../../public/fonts/Bookmania-Light.woff2', 
-            weight: '100', 
-            style: 'normal'
-        }, 
-        {
-            path: '../../public/fonts/Bookmania-LightItalic.woff2', 
-            weight: '100', 
-            style: 'italic'
-        }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-BlackItalic.woff2', 
+        //     weight: '900', 
+        //     style: 'italic'
+        // }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-Bold.woff2', 
+        //     weight: '700', 
+        //     style: 'normal'
+        // }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-BoldItalic.woff2', 
+        //     weight: '700', 
+        //     style: 'italic'
+        // }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-Light.woff2', 
+        //     weight: '100', 
+        //     style: 'normal'
+        // }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-LightItalic.woff2', 
+        //     weight: '100', 
+        //     style: 'italic'
+        // }, 
         {
             path: '../../public/fonts/Bookmania-Regular.woff2', 
             weight: '400', 
             style: 'normal'
         }, 
-        {
-            path: '../../public/fonts/Bookmania-RegularItalic.woff2', 
-            weight: '400', 
-            style: 'italic'
-        }, 
+        // {
+        //     path: '../../public/fonts/Bookmania-RegularItalic.woff2', 
+        //     weight: '400', 
+        //     style: 'italic'
+        // }, 
         {
             path: '../../public/fonts/Bookmania-Semibold.woff2', 
             weight: '500', 
             style: 'normal'
         }, 
-        {
-            path: '../../public/fonts/Bookmania-SemiboldItalic.woff2', 
-            weight: '500', 
-            style: 'italic'
-        }
+        // {
+        //     path: '../../public/fonts/Bookmania-SemiboldItalic.woff2', 
+        //     weight: '500', 
+        //     style: 'italic'
+        // }
     ]
 })
 
